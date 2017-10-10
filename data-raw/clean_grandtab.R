26804f33f8d7ebd4d69be88b13801bab83aefb65
thesadie <sgill@flowwest.com>
2017-10-10 16:53:02 -0700

    cleaning grandtab

--- data-raw/clean_grandtab.R
+++ data-raw/clean_grandtab.R
@@ -1,12 +1,38 @@
 library(tidyverse)
 library(stringr)
 
-#new data from jessica goes back further
+# jessica compiled data 1975-2015
 gt <- read_csv('data-raw/grandtab.csv', skip = 3)
 
 gt %>%
-  filter(DATA_COLLECTION_TYPE == 'In-River Count')
-  glimpse()
+  filter(DATA_COLLECTION_TYPE == 'In-River Count') %>%
+  group_by(RIVER_SYSTEM, RIVER, REACH) %>%
+  filter(!is.na(REACH)) %>%
+  summarise(count = n()) %>% View()
+  # glimpse()
+# sacramento is funky with reach river combo
+# what is 'Other' RIVER
+# Thomes misspelled Toomes
+
+gt %>%
+  mutate(river = case_when(
+    RIVER == 'Toomes Creek' ~ 'Thomes Creek',
+    RIVER == 'Sacramento River Main Stem' & REACH == 'Keswick Dam to RBDD' ~ 'Upper Sacramento River',
+    RIVER == 'Sacramento River Main Stem' & REACH == 'RBDD to Princeton Ferry' ~ 'Upper-mid Sacramento River',
+    TRUE ~ RIVER
+  )) %>%
+  filter(DATA_COLLECTION_TYPE == 'In-River Count') %>%
+  group_by(RIVER_SYSTEM, river, REACH) %>%
+  filter(!is.na(REACH)) %>%
+  summarise(count = n()) %>% View()
+
+gt %>%
+  filter(RIVER == 'Sacramento River Main Stem', RUN == 'Fall',
+         DATA_COLLECTION_TYPE == 'In-River Count') %>%
+  select(year = YEAR, type = DATA_COLLECTION_TYPE, river = RIVER,
+         reach = REACH, count = ESTIMATE, run = RUN) %>%
+  ggplot(aes(x = year, y = count, fill = reach)) +
+  geom_col(position = 'dodge')
 
 
 gt <- grandtab %>%
@@ -16,3 +42,10 @@ gt <- grandtab %>%
   select(year = YEAR, watershed, count = ESTIMATE, run = RUN, type = DATA_COLLECTION_TYPE)
 
 
+#compare to sac pass data from adam durante 2003-2016
+gt2 <- read_csv('data-raw/GrandTab2003_2016eachTrib.csv') %>%
+  gather(year, count, -Watershed.full) %>%
+  rename(watershed = Watershed.full) %>%
+  arrange(year)
+
+gt2
