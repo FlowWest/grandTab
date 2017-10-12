d9cbccf6bc2eccd736b18a815b722068436841cd
thesadie <sgill@flowwest.com>
2017-10-12 10:48:13 -0700

    compare adam to jessica data

--- data-raw/clean_grandtab.R
+++ data-raw/clean_grandtab.R
@@ -4,7 +4,7 @@ library(stringr)
 cvpia_watershed <- read_csv('data-raw/cvpia_trib_names.csv') %>% pull(watershed)
 
 # jessica compiled data 1975-2015
-gt <- read_csv('data-raw/grandtab.csv', skip = 3)
+gt <- read_csv('data-raw/grandtab1975_2015.csv', skip = 3)
 
 gt %>%
   filter(DATA_COLLECTION_TYPE == 'In-River Count') %>%
@@ -31,20 +31,24 @@ grandtab <- gt %>%
 use_data(grandtab)
 
 
-gt %>%
-  filter(RIVER == 'Sacramento River Main Stem', RUN == 'Fall',
-         DATA_COLLECTION_TYPE == 'In-River Count') %>%
-  select(year = YEAR, type = DATA_COLLECTION_TYPE, river = RIVER,
-         reach = REACH, count = ESTIMATE, run = RUN) %>%
-  ggplot(aes(x = year, y = count, fill = reach)) +
-  geom_col(position = 'dodge')
-
-
-
+# TODO
 #compare to sac pass data from adam durante 2003-2016
 gt2 <- read_csv('data-raw/GrandTab2003_2016eachTrib.csv') %>%
   gather(year, count, -Watershed.full) %>%
   rename(watershed = Watershed.full) %>%
   arrange(year)
 
-gt2
+gt1 <- grandtab %>%
+  filter(type == 'Natural', run == 'Fall') %>%
+  mutate(source = 'Jessica') %>%
+  select(year, count, watershed, source)
+
+gt2 %>%
+  mutate(source = 'Adam', year = as.numeric(year)) %>%
+  select(year, count, watershed, source) %>%
+  bind_rows(gt1) %>%
+  filter(watershed == 'Upper Sacramento River') %>%
+  ggplot(aes(x = year, y = count, fill = source)) +
+  geom_col(position = 'dodge')
+
+  write_csv(grandtab, 'grandtab.csv')
